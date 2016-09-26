cd1dcbe424708cc0dda1ef1200dd6321c5987824
garrettwegan <garrettegan@austin.rr.com>
2016-09-26 09:06:15 -0500

    Add files via upload

--- src/brickbreaker/HighScoresScreen.java
+++ src/brickbreaker/HighScoresScreen.java
@@ -8,9 +8,9 @@ package brickbreaker;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
-import java.awt.Graphics2D;
+///import java.awt.Graphics2D;
 import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
+//import java.awt.event.KeyListener;
 import java.awt.event.MouseEvent;
 import java.awt.image.BufferedImage;
 import java.io.BufferedReader;
@@ -22,57 +22,53 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
-import java.util.Scanner;
+//import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
  *
- * @author ge29779
+ * @author Garrett Egan
  */
 public class HighScoresScreen extends AbstractScreen implements Comparator<String>{
 
-    //variables
+    //Constants
+    //------------------------------------------------------------------
+    private final int maxNameSize = 6;
+    private final String fileName = "src\\assets\\HighScoresFile";
+    
+    //Variables
     //------------------------------------------------------------------
     private int currentScore;
     private boolean typing;
     private String playerName;
     private ArrayList<String> scoresList;
-    private final String fileName;
     private BufferedImage background;
-        
-    //constructor receiving the score and setting the variables
+            
+    //Default constructor called from menu and only displays previous scores
+    //------------------------------------------------------------------
+    public HighScoresScreen(){
+        super();
+        typing = false;
+        scoresList = new ArrayList<String>();
+        addScores();
+    }
+    
+    //Constructor with score called when coming out of play, need to enter a name
     //------------------------------------------------------------------
     public HighScoresScreen(int currentScore){
         super();
-        this.currentScore = currentScore;
         typing = true;
         playerName = "";
-        fileName = "src\\assets\\HighScoresFile";
+        this.currentScore = currentScore;
         scoresList = new ArrayList<String>();
         addScores();
-        //Scanner scan = new Scanner(System.in);
-        //scoresList.add(scan.nextLine());
-        //sortScores();
-        //Collections.reverse(scoresList);
-        //writeScores();
     }
     
-    
+    //Not used
+    //------------------------------------------------------------------
     @Override
-    public void runLogic() {
-        if(typing){
-            return;
-            
-        }
-        else{
-            sortScores();
-            Collections.reverse(scoresList);
-            writeScores();
-            System.out.println(scoresList);
-            
-        }
-    }
+    public void runLogic() {}
 
     @Override
     public void drawGame(Graphics g) {
@@ -82,46 +78,84 @@ public class HighScoresScreen extends AbstractScreen implements Comparator<Strin
         g.fillRect(0, 0, getWidth(), getHeight());
         
         g.setColor(Color.WHITE);
+        Font font =  new Font("Garamond", Font.PLAIN, 24);
+        g.setFont(font);
+        
         
         if(typing){            
+            //prompt player to input name
+            drawCenteredString(g, "What is your name?", getWidth() / 2, getHeight() / 4, font);
+            
             //Draw playerName
-            drawCenteredString(g, playerName, 400, 300, new Font("Garamond", Font.PLAIN, 24));
+            drawCenteredString(g, playerName, getWidth() / 2, getHeight() / 2, font);
         }
         
         else{
-            //Draw playerName
-            drawCenteredString(g, scoresList.toString(), 400, 300, new Font("Garamond", Font.PLAIN, 24));
+            
+            //Draw top 10 scores
+            for(int i = 0; i < 10 && i < scoresList.size(); i++){
+                g.drawString((i + 1) + ". " + scoresList.get(i), getWidth() / 3, (getHeight() / 2) + (i * g.getFont().getSize()));
+            }
         }
     }
 
     
-    //Handles name input
-    //------------------------------------------------------------------
+    
     @Override
     public void keyTyped(KeyEvent e) {}
 
     @Override
     public void keyPressed(KeyEvent e) {}
 
+    //Handles name input
+    //------------------------------------------------------------------
     @Override
     public void keyReleased(KeyEvent e) {
         //System.out.print("Key Typed");
         int key = e.getKeyCode();
+        
+        //Except input if typing
+        //------------------------------------------------------------------
         if(typing){
             if(key == KeyEvent.VK_BACK_SPACE){
                 playerName = playerName.substring(0, playerName.length()-1);
-                System.out.println("BackSpace");
+              if(getEnableMasterDebug()){
+                  System.out.println("BackSpace");
+              }
             }
             else if(key == KeyEvent.VK_ENTER){
                 typing = false;
                 scoresList.add(currentScore + " " + playerName);
-                System.out.println("Enter");
+                writeScores();
+                
+              if(getEnableMasterDebug()){
+                  System.out.println("Enter");
+              }
             }
-            else if(Character.isLetterOrDigit(key)){
+            else if(Character.isLetterOrDigit(key) && playerName.length() < maxNameSize){
                 playerName += (char)key;
-                System.out.println("" + (char)key);
+
+              if(getEnableMasterDebug()){
+                  System.out.println("" + (char)key);
+              }                
             }
-            //System.out.print("In typing");
+        }
+        if(key == KeyEvent.VK_F10){
+            //enter playgame with or without saving
+            
+            
+              if(getEnableMasterDebug()){
+                  System.out.println("" + (char)key);
+              }  
+            
+        }
+        if(key == KeyEvent.VK_ESCAPE){
+            //enter mainMenu with or without saving\
+            
+            
+              if(getEnableMasterDebug()){
+                  System.out.println("" + (char)key);
+              }  
         }
     }
     
@@ -129,6 +163,7 @@ public class HighScoresScreen extends AbstractScreen implements Comparator<Strin
     //------------------------------------------------------------------
     public void sortScores(){
         scoresList.sort(this);
+        Collections.reverse(scoresList);
     }
 
     //Compares strings of score/name combos and compares by score
@@ -139,9 +174,9 @@ public class HighScoresScreen extends AbstractScreen implements Comparator<Strin
             return 1;
         }
         else if(Integer.parseInt(o1.substring(0, o1.indexOf(' '))) == Integer.parseInt(o2.substring(0, o1.indexOf(' '))) ){
-            if(o1.substring(o1.indexOf(' '), o1.length()).compareTo(o2.substring(o2.indexOf(' '), o2.length())) == 1)
+            if(o1.substring(o1.indexOf(' '), o1.length()).compareTo(o2.substring(o2.indexOf(' '), o2.length())) > 0)
                 return -1;
-            else if(o1.substring(o1.indexOf(' ')).compareTo(o2.substring(o2.indexOf(' '))) == -1)
+            else if(o1.substring(o1.indexOf(' ')).compareTo(o2.substring(o2.indexOf(' '))) < 0)
                 return 1;
             else
                 return 0;
@@ -154,6 +189,8 @@ public class HighScoresScreen extends AbstractScreen implements Comparator<Strin
         }
     }
 
+    //Read scores in and add them to scoresList
+    //------------------------------------------------------------------
     private void addScores() {
         
         //Uses declared final name
@@ -184,7 +221,12 @@ public class HighScoresScreen extends AbstractScreen implements Comparator<Strin
         }
     }
     
+    //Write scoresList to the HighScoresFile
+    //------------------------------------------------------------------
     private void writeScores(){
+        
+        //Sort scores before writing
+        sortScores();
         
         //Uses declared file name
         
@@ -215,7 +257,5 @@ public class HighScoresScreen extends AbstractScreen implements Comparator<Strin
     public void mouseMoved(MouseEvent e) {}
 
     @Override
-    public void handleInput(ArrayList<KeyEvent> inputList) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+    public void handleInput(ArrayList<KeyEvent> inputList) {}
 }
